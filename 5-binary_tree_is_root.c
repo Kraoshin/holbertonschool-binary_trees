7801e95ede35a2765e2ef2868875285c028d0089
kraoshin <killian.kerleau@outlook.fr>
2024-12-09 11:36:37 +0100

    add betty style

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,7 +1,11 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_is_root - check if node is a root
  *
+ * @node: pointer to the node we're checking
+ *
+ * Return: 0 if not a node 1 otherwise
  */
 
 int binary_tree_is_root(const binary_tree_t *node)
